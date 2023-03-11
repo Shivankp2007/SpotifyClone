252e5d6c0e761daf1924e9ec59df0dcba5b34010
Shivankp2007 <114836142+Shivankp2007@users.noreply.github.com>
2023-03-11 15:48:11 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Route,Routes,Switch } from "react-router-dom";
+import SearchPg from "./Components/SearchPg/SearchPg";
+import Homepg from "./Homepg";
+import SideNav from "./Components/SideNav";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideNav />
+      <Routes>
+        <Route path="/" Component={Homepg} />
+        <Route path="/search" Component={SearchPg} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
